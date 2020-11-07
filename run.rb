a0e650b04239017f2879fafed4814edb8b150cb4
BMoClay <benmorgancleveland@gmail.com>
2020-11-07 16:24:03 -0500

    first commit

--- run.rb
+++ run.rb
@@ -6,5 +6,12 @@ require 'pry'
 #Test your code here
 
 
+bronx_zoo = Zoo.new("Bronx Zoo", "The Bronx")
+stone_zoo = Zoo.new("Stone Zoo", "Boston")
+lion = Animal.new("lion", 280, "Chonky", bronx_zoo)
+sloth = Animal.new("sloth", 90, "slinky", stone_zoo)
+mouse = Animal.new("mouse", 2, "cutsie", bronx_zoo)
+mouse2 = Animal.new("mouse", 2, "boopsie", bronx_zoo)
+
 binding.pry
 puts "done"
